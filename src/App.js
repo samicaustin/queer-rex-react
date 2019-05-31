bf86573d3d656051d63760cda208f39996b946f8
Sami Austin <samicaustin@gmail.com>
2019-05-31 10:31:38 -0600

    Finished adding new routes

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
   }
 
   handleRegister = async (formData) => {
-    const response = await fetch("http://localhost:8080/users", {
+    const response = await fetch("https://queerrex-java.herokuapp.com/users", {
         method: "POST",
         body: JSON.stringify(formData),
         headers: {
@@ -31,7 +31,7 @@ class App extends Component {
   }
 
   handleLogin = async (formData) => {
-      const response = await fetch("http://localhost:8080/login", {
+      const response = await fetch("https://queerrex-java.herokuapp.com/login", {
           method: "POST",
           body: JSON.stringify(formData),
           headers: {
